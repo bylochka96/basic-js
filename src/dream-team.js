d2e396f74012505800acd7a4570f208ad83770fc
Gutnik Efim <bylochka96@gmail.com>
2021-09-07 18:21:39 +0300

    done: dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,19 +1,32 @@
-import { NotImplementedError } from '../extensions/index.js';
+import {NotImplementedError} from '../extensions/index.js';
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-export default function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function createDreamTeam(members) {
+    if (Array.isArray(members)) {
+        let tempArr = [];
+        for (let member of members) {
+            if (typeof (member) === 'string') {
+                tempArr.push((member.trim())[0].toUpperCase());
+            }
+        }
+        tempArr.sort((a, b) => {
+            if (a < b) return -1;
+            if (a > b) return 1;
+            return 0;
+        });
+        return tempArr.join("");
+    }
+    return false;
 }
